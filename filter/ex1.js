9fc23a43f4c7b6987f7b2908748b9e1578b0d39c
bspiri <54644338+bspiri@users.noreply.github.com>
2020-03-24 12:34:12 +0200

    update filter/ex1/js

--- filter/ex1.js
+++ filter/ex1.js
@@ -13,8 +13,8 @@ Expected OUTPUT for this sample:
 
 */
 
-function getPositiveNumbers(numbers) {
-}
+const numbers = [-2, -1, 0, 1, 2];
+const getPositiveNumbers = numbers.filter(number => number >= 0);
 
 // If you need, you can always visualize the result with a console.log.
 // In that case, uncomment the line below (you can also add your own
@@ -22,7 +22,7 @@ function getPositiveNumbers(numbers) {
 // Instead of running `npm test`, you can then use `node filter/ex1`
 // (you still need to run `npm test` to make sure you did right).
 
-// console.log(getPositiveNumbers([-2, -1, 0, 1, 2]));
+console.log(getPositiveNumbers);
 
 // DON'T TOUCH THIS!
 module.exports = getPositiveNumbers;
