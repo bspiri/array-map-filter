9166f4596103ba74dc447732cf99d86a718c3041
bspiri <54644338+bspiri@users.noreply.github.com>
2020-03-24 13:42:00 +0200

    update filter/ex1.j

--- filter/ex1.js
+++ filter/ex1.js
@@ -14,15 +14,16 @@ Expected OUTPUT for this sample:
 */
 
 const numbers = [-2, -1, 0, 1, 2];
-const getPositiveNumbers = numbers.filter(number => number >= 0);
-
+function getPositiveNumbers(number) {
+  return number >= 0;
+}
 // If you need, you can always visualize the result with a console.log.
 // In that case, uncomment the line below (you can also add your own
 // console.log INSIDE the function).
 // Instead of running `npm test`, you can then use `node filter/ex1`
 // (you still need to run `npm test` to make sure you did right).
 
-console.log(getPositiveNumbers);
+console.log(numbers.filter(getPositiveNumbers));
 
 // DON'T TOUCH THIS!
 module.exports = getPositiveNumbers;
